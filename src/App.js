a2a703568290dbc50f0143ceaa59d6bb89034f6e
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-11-04 11:17:05 +0100

    add the component from the vanilla project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Header from './components/Header.jsx';
+import Figure from './components/Figure.jsx';
+import WrongLetters from './components/WrongLetters.jsx';
+import Word from './components/Word.jsx';
 
 function App() {
   return (<>
-
+    <Header />
+    <div className='game-container'>
+      <Figure />
+      <WrongLetters />
+      <Word />
+    </div>
   </>
   );
 }
